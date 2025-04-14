ea3c2a9b2c8fe913376ee269c4e4b26279027ef9
TrentLangford <69657165+TrentLangford@users.noreply.github.com>
2025-04-14 09:15:20 -0500

    fixed broken import

---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour
+{
+    public GameObject SpawningPrefab;
+    public bool SpawnObject;
+    public bool UnhideChild;
+    public void Complete()
+    {
+
+    }
+}
